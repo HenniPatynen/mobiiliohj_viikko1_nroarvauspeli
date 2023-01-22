9d33189d30c393010bcc021e5e25afa542e32bbd
Henni Pätynen <henni.patynen@myy.haaga-helia.fi>
2023-01-22 20:32:24 +0200

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, Button, Alert } from 'react-native';
+import { useState } from 'react';
+import { Text } from 'react-native';
+
+var oikea = Math.floor(Math.random() * 100) + 1;
+var lkm =1;
 
 export default function App() {
+
+  const [arvaus, setArvaus] = useState('');
+
+  const [text, setText]=useState('');
+
+  const pressed =()=>{
+    if(arvaus<oikea){
+     setText('Arvauksesi on liian matala, arvaa korkeampaa lukua')
+      lkm=lkm +1;
+    }else if(arvaus>oikea){
+      setText('Arvauksesi on liian korkea, arvaa pienempää lukua')
+      lkm=lkm+1;
+    }else{
+      Alert.alert('Arvasit oikean luvun ' + lkm + ' arvauksella')
+      oikea = Math.floor(Math.random() * 100) + 1;
+      setArvaus('');
+      setText('');
+      lkm=1;
+    }
+};
+console.log(oikea)
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style ={{fontSize:20}}>Arvaa jokin luku 1-100 välillä</Text>
+      <Text> {text} </Text>
+      
+      <TextInput
+      style={styles.teksti}
+      onChangeText={nro=> setArvaus(nro)}
+      value={arvaus}
+      />
+
+      <Button 
+      title ="Arvaa" 
+      onPress={pressed}/>
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +58,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  teksti: {
+    width:200, 
+    borderColor:'grey', 
+    borderWidth:2, 
+    marginBottom: 10
+  }
 });
+
